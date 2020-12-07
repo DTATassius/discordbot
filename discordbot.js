30cd7300f332b5865cd3870bc258ef420c62569f
DTATassius <74790936+DTATassius@users.noreply.github.com>
2020-12-07 20:27:56 +0400

    Update discordbot.js

--- discordbot.js
+++ discordbot.js
@@ -37,7 +37,7 @@ client.on("message", message =>{
 
             }
          
-            else if(message.guild.roles.cache.some(role => role.name !== roleSelected)){
+            else if(message.guild.roles.cache.has(role => role.name === roleSelected)){
                     message.reply("This isn't a role, dumbass.");
             }
 
