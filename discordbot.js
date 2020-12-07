c0ffac029e55dc42d3f6688ad243ab20df5292f4
DTATassius <74790936+DTATassius@users.noreply.github.com>
2020-12-07 21:43:49 +0400

    Update discordbot.js

--- discordbot.js
+++ discordbot.js
@@ -25,6 +25,7 @@ client.on("message", message =>{
     }
     
     if(message.content.startsWith(prefix + "giverole ")){
+        message.reply(message.guild.roles.cache.has(role => role.name === roleSelected));
       
         try{
 
