93d7d8f5504ddb069174f975667a153ee3d6480b
DTATassius <74790936+DTATassius@users.noreply.github.com>
2021-05-03 12:31:37 +0400

    Comments
    Added comments to help with reading the bot's horror-code

--- discordbot.js
+++ discordbot.js
@@ -1,8 +1,8 @@
 const token = require("./settings.json");
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client(); /* Adding files */
 
-var prefix = "$"
+var prefix = "$" /* Prefix for the bot */
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
@@ -11,29 +11,29 @@ client.on('ready', () => {
 client.on("message", message =>{
 
     var guild = message.guild;
-    var roleSelected = message.content.slice(10,message.content.length);
+    var roleSelected = message.content.slice(10,message.content.length); 
     console.log('Recieved and read the following message: ');
-    console.log(message.content);
+    console.log(message.content); /* Cuts and logs the message that was written */
  
     if(message.content.toLowerCase().includes("bot" || "bots") && message.content.toLowerCase().includes("broken")){
-     message.reply("I am _really_ broken though?");
+     message.reply("I am _not_ broken");
      console.log("someone dissed my bot");
     }
     else if(message.content.toLowerCase().includes("bot" || "bots") && message.content.toLowerCase().includes("stupid")){
      message.reply("I'm not _that_ stupid you know.");
-     console.log("The bot is actually stupid though");
+     console.log("The bot is actually stupid though"); /* Funny jokes when someone disses my bot */
     }
     
-    if(message.content.startsWith(prefix + "giverole ")){
+    if(message.content.startsWith(prefix + "giverole ")){ /* Giverole function produced by calling $giverole action */
       
         try{
 
-            const role = message.guild.roles.cache.find(role => role.name === roleSelected);
+            const role = message.guild.roles.cache.find(role => role.name === roleSelected); /* Looks for the role that was requested */
        
             if(message.member.roles.cache.some(role => role.name === roleSelected)) {
 
                 message.reply(`You've already got the role bud.`);
-                console.log("User already had the role: " + roleSelected);
+                console.log("User already had the role: " + roleSelected); /* User already has the role */
 
             }
 
@@ -43,7 +43,7 @@ client.on("message", message =>{
             let member = message.guild.member(message.author);
             message.guild.member(message.author).roles.add(role);
             message.reply("Added the role!");
-            console.log("Added the role: " + roleSelected + " to User");
+            console.log("Added the role: " + roleSelected + " to User"); /* Adds the role to the user */
 
             }
 
@@ -53,11 +53,10 @@ client.on("message", message =>{
 
             message.reply("This either is not a role, or your spelling is off.");
             message.reply("Try sending it again.");
-            message.reply("If I'm actually broken, message me on discord.");
+            message.reply("If I'm actually broken, message me on discord."); /* Someone didn't fix the bot and the person messed up in typing the role */
             console.log(err);
 
         }
-        message.reply("7bot work7");
         if(message.guild.roles.cache.get(role => role.name !== roleSelected) !== undefined){
          message.reply("It is not undefined");
         }
@@ -65,11 +64,10 @@ client.on("message", message =>{
          message.reply("It is undefined");
         }
         else{
-          message.reply("It is neither defined or undefined");
+          message.reply("It is neither defined or undefined"); /* ***BOT IS STILL IN PROGRESS*** */
         }
-        message.reply("7bot still work7");
     }
 
 });
 
-client.login(token.token);
+client.login(token.token); /* Logs the bot into Discord */
