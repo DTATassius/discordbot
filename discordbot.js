2bb37076ff0eb59f2d25d86b70cec08c1e51e916
DTATassius <74790936+DTATassius@users.noreply.github.com>
2021-05-04 09:15:12 +0400

    Update discordbot.js

--- discordbot.js
+++ discordbot.js
@@ -15,7 +15,7 @@ client.on("message", message =>{
     var roleSelected = message.content.slice(10,message.content.length); 
     console.log('Recieved and read the following message: ');
     console.log(message.content); /* Cuts and logs the message that was written */
-    fs.writeFile("Sever_Logs.txt", message.content, console.log("Failed to write text")); /* Test code for logging messages sent to guilds */
+    fs.writeFile("Sever_Logs.txt", message.content); /* Test code for logging messages sent to guilds */
  
     if(message.content.toLowerCase().includes("bot" || "bots") && message.content.toLowerCase().includes("broken")){
      message.reply("I am _not_ broken");
